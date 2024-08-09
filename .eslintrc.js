dd12ac0be77aae83d005b0782740e09203d6335f
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-09 20:36:54 +0900

    feat: eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,16 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "off",
+        "printWidth": 200,
+        "singleQuote": true,
+        "trailingComma": "all",
+        "skipBlankLines": true,
+        "ignoreComments": true
+      }
+    ]
   },
 };
