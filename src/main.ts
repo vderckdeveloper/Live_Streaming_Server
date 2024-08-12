d0c519a716a42e00851b8cfdd7ab767dc487160b
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-12 19:52:08 +0900

    feat: main ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   const port = process.env.SERVER_PORT;
+
+  // serve static files
+  app.useStaticAssets(join(__dirname, '..', '..', '..', 'uploads'), { prefix: '/uploads' });
+
+  // validation pie
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  // trust proxy
+  app.set('trust proxy', process.env.NODE_ENV === 'development' ? 1 : 2);
+
+  // enable cors
+  app.enableCors({
+    origin: process.env.SOCKET_IO_HOST_ORIGIN,
+    credentials: true,
+    methods: ['GET', 'POST'],
+  });
+
 
   await app.listen(port);
 }
