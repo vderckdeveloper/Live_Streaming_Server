59c64413b16ab0cbffb5df2047a678019eff1ffb
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-12 19:51:39 +0900

    feat: config module, socket module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+// socket module
+import { SocketModule } from './socket/socket.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'development' ? '.env.development' : '.env.production',
+    }),
+    SocketModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
