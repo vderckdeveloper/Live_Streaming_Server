cdfedbb178fce36bbdf3c29c83abaaa158d9fd2a
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-12 23:08:25 +0900

    feat: room name test

---
+++ src/roomname/roomName.service.spec.ts
@@ -0,0 +1,50 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { RoomNameService } from './roomName.service';
+
+describe('RoomNameService', () => {
+  let service: RoomNameService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [RoomNameService],
+    }).compile();
+
+    service = module.get<RoomNameService>(RoomNameService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('setRoom', () => {
+    it('should correctly map a socket ID to a room', () => {
+      service.setRoom('socket1', 'room1');
+      expect(service.getRoom('socket1')).toEqual('room1');
+    });
+  });
+
+  describe('getRoom', () => {
+    it('should return undefined if the socket ID is not mapped', () => {
+      expect(service.getRoom('nonexistent')).toBeUndefined();
+    });
+  });
+
+  describe('deleteRoom', () => {
+    it('should remove the socket ID from the mapping', () => {
+      service.setRoom('socket1', 'room1');
+      service.deleteRoom('socket1');
+      expect(service.getRoom('socket1')).toBeUndefined();
+    });
+  });
+
+  describe('hasRoom', () => {
+    it('should return true if the socket ID is in the mapping', () => {
+      service.setRoom('socket1', 'room1');
+      expect(service.hasRoom('socket1')).toBe(true);
+    });
+
+    it('should return false if the socket ID is not in the mapping', () => {
+      expect(service.hasRoom('nonexistent')).toBe(false);
+    });
+  });
+});
