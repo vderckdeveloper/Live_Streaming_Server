585bc79f7a2d8cfb010ac4c02a522914cc66582a
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-12 23:07:46 +0900

    feat: room name module

--- src/socket/socket.module.ts
+++ src/socket/socket.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SocketGateway } from './socket.gateway';
 
+import { RoomNameModule } from 'src/roomname/roomName.module';
+
 @Module({
-  imports: [],
+  imports: [RoomNameModule],
   providers: [SocketGateway],
 })
 export class SocketModule { }
