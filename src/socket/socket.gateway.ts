43b807e8bee7ce3cc9f3dbb174415f1609e12111
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-15 22:27:31 +0900

    feat: web rtc multiconnection

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -23,6 +23,8 @@ interface RoomCode {
 
 interface SDPMessage {
     roomCode: string;
+    offerId ?: string;
+    peerId ?: string;
 }
 
 interface ICECandidate {
@@ -73,7 +75,7 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
         // If room members exceed 2, disconnect the socket
         const roomMember = this.roomNameService.getMembers(roomCode);
-        if (roomMember.length >= 2) {
+        if (roomMember.length >= 4) {
             socket.emit('error', 'Room is full');
             socket.disconnect(true);
             return;
@@ -87,35 +89,47 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
         // join room
         socket.join(roomCode);
+
+        // Filter out the current socket ID from the room members
+        const otherMembers = roomMember.filter(peerSocketId => peerSocketId !== socket.id);
+
+        // send peer socket id to the later participant
+        otherMembers.forEach(peerSocketId => {
+            socket.emit('register', { offerId: socket.id, peerId: peerSocketId });
+        });
     }
 
     @SubscribeMessage('candidate')
-    async handleCandidate(socket: any,ICEcandidate: ICECandidate) {
-        // candidate message sent
-        console.log('candidate', ICEcandidate);
-
+    async handleCandidate(socket: any, ICEcandidate: ICECandidate) {
+        // // candidate message sent
+        // console.log('candidate', ICEcandidate);
         const roomCode = this.memberRoomService.getMemberRoom(socket.id);
+        const roomMember = this.roomNameService.getMembers(roomCode);
+
+        // Filter out the current socket ID from the room members
+        const otherMembers = roomMember.filter(peerSocketId => peerSocketId !== socket.id);
 
         // Broadcast the ICE candidate to all other clients in the room except the sender
-        socket.to(roomCode).emit('candidate', ICEcandidate);
+        otherMembers.forEach(peerSocketId => {
+            socket.to(peerSocketId).emit('candidate', ICEcandidate);
+        });
     }
 
     @SubscribeMessage('offer')
     async handleOffer(socket: any, SDPmessage: SDPMessage) {
-        const roomCode = this.memberRoomService.getMemberRoom(socket.id);
+        // peer id
+        const { peerId } = SDPmessage;
 
-        // Broadcast the offer to all other clients in the room except the sender
-        socket.to(roomCode).emit('offer', SDPmessage);
+        // Broadcast the ICE candidate to all other clients in the room except the sender
+        socket.to(peerId).emit('offer', SDPmessage);
     }
 
     @SubscribeMessage('answer')
     async handleAnswer(socket: any, SDPmessage: SDPMessage) {
-        // answer
-        console.log('candidate', SDPmessage);
+        // offer id
+        const { offerId } = SDPmessage; 
 
-        const roomCode = this.memberRoomService.getMemberRoom(socket.id);
-
-        // Broadcast the answer to all other clients in the room except the sender
-        socket.to(roomCode).emit('answer', SDPmessage);
+        // Broadcast the ICE candidate to all other clients in the room except the sender
+        socket.to(offerId).emit('answer', SDPmessage);
     }
 }
