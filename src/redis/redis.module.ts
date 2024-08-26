d060b70fb4858d41bc5227769c7fcb2756c5c30c
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-26 20:40:50 +0900

    feat: redis module

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,9 @@
+// redis.module.ts
+import { Module } from '@nestjs/common';
+import { RedisService } from './redis.service';
+
+@Module({
+    providers: [RedisService],
+    exports: [RedisService],
+})
+export class RedisModule {}
