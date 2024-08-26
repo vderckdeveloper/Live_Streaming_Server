7df3840f56ad3cdaf207e88935f14357e809fc9c
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-26 20:40:43 +0900

    refractor: redis optmized structure

--- src/memberroom/memberRoom.service.ts
+++ src/memberroom/memberRoom.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { RedisService } from '../redis/redis.service';
 
 @Injectable()
 export class MemberRoomService {
-    private memberRoomName: Map<string, string> = new Map();
+    private redisClientV4;
+
+    constructor(private redisService: RedisService) { }
+
+    async onModuleInit() {
+        const redisClient = await this.redisService.getClient();
+        this.redisClientV4 = redisClient.v4;
+    }
 
     // Set or update the room with a value
-    setMemberRoom(socket: string, room: string): void {
-        this.memberRoomName.set(socket, room);
+    async setMemberRoom(socket: string, room: string): Promise<void> {
+        await this.redisClientV4.set(socket, room);
     }
 
     // Retrieve the value of the room
-    getMemberRoom(socket: string): string | undefined {
-        return this.memberRoomName.get(socket);
+    async getMemberRoom(socket: string): Promise<string | null> {
+        return await this.redisClientV4.get(socket);
     }
 
     // Delete a room entry
-    deleteMemberRoom(socket: string): void {
-        this.memberRoomName.delete(socket);
+    async deleteMemberRoom(socket: string): Promise<void> {
+        await this.redisClientV4.del(socket);
     }
 
     // Check if the room exists
-    hasMemberRoom(socket: string): boolean {
-        return this.memberRoomName.has(socket);
+    async hasMemberRoom(socket: string): Promise<boolean> {
+        const result = await this.redisClientV4.exists(socket);
+        return result === 1;
     }
 }
