df775bde5d0193e8ee4eea8f9e1352ada326686f
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-26 20:41:07 +0900

    feat: redis module

--- src/roomname/roomName.module.ts
+++ src/roomname/roomName.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RoomNameService } from './roomName.service';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
+    imports: [RedisModule],
     providers: [RoomNameService],
     exports: [RoomNameService] 
 })
