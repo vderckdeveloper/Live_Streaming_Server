cb66357001317594d2b9998680f421df560c25ea
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-26 20:40:26 +0900

    feat: redis module

--- src/memberroom/memberRoom.module.ts
+++ src/memberroom/memberRoom.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MemberRoomService } from './memberRoom.service';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
+    imports: [RedisModule],
     providers: [MemberRoomService],
     exports: [MemberRoomService] 
 })
